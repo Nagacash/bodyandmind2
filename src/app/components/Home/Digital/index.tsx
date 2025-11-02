a3ec965d4a726dda8884e80be4bc26bfb1d4482c
Moe <chosenfewrecords@hotmail.de>
2025-11-02 15:20:14 +0100

    first commit

--- src/app/components/Home/Digital/index.tsx
+++ src/app/components/Home/Digital/index.tsx
@@ -11,17 +11,17 @@ const Digital = () => {
   return (
     <section className='relative bg-cover bg-center overflow-hidden py-16'>
       <div className='container mx-auto max-w-7xl px-4'>
-                  <div className="rounded-3xl bg-blue-100 bg-no-repeat bg-right-top lg:pb-40 pb-20 relative overflow-hidden shadow-lg">          <div className='grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-2'>
+                  <div className="rounded-3xl bg-blue-100 bg-no-repeat bg-right-top lg:pb-40 pb-20 relative overflow-hidden shadow-lg flex flex-col items-center justify-center">
             {/* COLUMN-1 */}
             <motion.div
               initial={{ opacity: 0, x: -50 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.8 }}
               className='pt-24 lg:pl-24'>
-              <p className='text-lg font-normal text-black mb-5 tracking-widest text-center lg:text-start uppercase mt-5'>
+              <p className='text-lg font-normal text-black mb-5 tracking-widest uppercase mt-5'>
               Ihr Fitness Trainer
               </p>
-              <h2 className={`text-white mb-8 leading-tight text-center lg:text-start lg:w-full ${bebasNeue.className}`}>
+              <h2 className={`text-white mb-8 leading-tight text-center lg:w-full ${bebasNeue.className}`}>
               
               Sind Sie bereit <span className='text-black'>etwas zu Ändern?</span> <br></br>
               Starten Sie jetzt und erreichen gemeinsam mit <span className='text-black'>unserem Experten Team</span>, Ihre Gesundheits- und Fitnessziele.            </h2>
@@ -33,7 +33,6 @@ const Digital = () => {
                 </Link> */}
               </div>
             </motion.div>
-          </div>
           {/* COLUMN-2 */}
           <motion.div
             initial={{ opacity: 0, x: 50 }}
